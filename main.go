d3c1d17a09e8c36b0b7814509627380a4d4fada2
pikomonde <32364823+pikomonde@users.noreply.github.com>
2017-10-07 03:46:24 +0700

    Early testing master (#3)
    * add vendor
    
    * add line api

--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	line "github.com/pikomonde/fam100bot/src/line"
 )
 
 func main() {
 	r := gin.New()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", ping)
+
+	// Line API
+	r.GET("/line/ping", line.Ping)
+
 	r.Run()
+}
+
+func ping(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
 }
