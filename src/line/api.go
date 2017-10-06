3a857394464d97baf7723e6e70777aeefdbad831
pikomonde <32364823+pikomonde@users.noreply.github.com>
2017-10-07 04:13:22 +0700

    validation and log test (#6)

--- src/line/api.go
+++ src/line/api.go
@@ -1,9 +1,33 @@
 package line
 
-import "github.com/gin-gonic/gin"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Webhook(c *gin.Context) {
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+
+	}
+	decoded, err := base64.StdEncoding.DecodeString(c.Request.Header.Get("X-Line-Signature"))
+	if err != nil {
+
+	}
+	hash := hmac.New(sha256.New, []byte("<channel secret>"))
+	hash.Write(body)
+
+	hmac.Equal(decoded, hash.Sum(nil))
+
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
+	fmt.Println("===> A")
+	log.Println("===> B")
 }
