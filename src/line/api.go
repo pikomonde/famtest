a16425b7f152c009d77b2f9d618ba5e320fe1fea
pikomonde <32364823+pikomonde@users.noreply.github.com>
2017-10-07 04:23:55 +0700

    see content of body (#7)

--- src/line/api.go
+++ src/line/api.go
@@ -1,6 +1,7 @@
 package line
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/base64"
@@ -14,20 +15,23 @@ import (
 func Webhook(c *gin.Context) {
 	body, err := ioutil.ReadAll(c.Request.Body)
 	if err != nil {
-
+		log.Println(err)
+		return
 	}
 	decoded, err := base64.StdEncoding.DecodeString(c.Request.Header.Get("X-Line-Signature"))
 	if err != nil {
-
+		log.Println(err)
+		return
 	}
 	hash := hmac.New(sha256.New, []byte("<channel secret>"))
 	hash.Write(body)
 
 	hmac.Equal(decoded, hash.Sum(nil))
 
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
+	//c.JSON(200, gin.H{
+	//	"message": "pong",
+	//})
 	fmt.Println("===> A")
-	log.Println("===> B")
+	n := bytes.IndexByte(body, 0)
+	fmt.Println(string(body[:n]))
 }
